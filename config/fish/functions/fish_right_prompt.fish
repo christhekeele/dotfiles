b6cb6f778640486c303e4428423e9b615f32c73f
Chris Keele <christhekeele@gmail.com>
2021-06-26 17:02:52 -0700

    parameterize right prompt

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,132 +1,208 @@
 function fish_right_prompt --description 'Write out the prompt'
-
-  set last $status
+  
   echo -n -s ' '
 
-  # Render right chevrons
+  set -l reset_color normal
+  set -l background_reset_color normal
+  
+  ####
+  # GIT INDEX status
+  ##
+  set -l git_index_badge '❮'
+
+  set -l none_staged_color red
+  set -l partially_staged_color yellow
+  set -l fully_staged_color green
+  set -l nothing_to_stage_color magenta
 
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1
 
-    # Use git-index color-coded chevrons
+    # Use git-index color-coded git_index_badges
 
     ####
     # Index status
     ##
 
-    set deletions_staged        0
-    set deletions_unstaged      0
-    set deletions_ours          0
-    set deletions_theirs        0
+    set -l deletions_staged        0
+    set -l deletions_unstaged      0
+    set -l deletions_ours          0
+    set -l deletions_theirs        0
 
-    set additions_staged        0
-    set additions_unstaged      0
-    set additions_ours          0
-    set additions_theirs        0
+    set -l additions_staged        0
+    set -l additions_unstaged      0
+    set -l additions_ours          0
+    set -l additions_theirs        0
 
-    set modifications_staged    0
-    set modifications_unstaged  0
-    set modifications_ours      0
-    set modifications_theirs    0
+    set -l modifications_staged    0
+    set -l modifications_unstaged  0
+    set -l modifications_ours      0
+    set -l modifications_theirs    0
 
     git status --porcelain --ignore-submodules=none | while read --delimiter "\n" -l line
 
       # Compute deletions
-      string match -q -r "^D[\sMT]\s*" $line;          and set deletions_staged (math $deletions_staged + 1)
-      string match -q -r "^[\sACMRT]D\s*" $line;       and set deletions_unstaged (math $deletions_unstaged + 1)
-      string match -q -r "^D[DU]\s*" $line;            and set deletions_ours (math $deletions_ours + 1)
-      string match -q -r "^[DU]D\s*" $line;            and set deletions_theirs (math $deletions_theirs + 1)
+      string match -q -r "^D[\sMT]\s*" $line;          and set -l deletions_staged (math $deletions_staged + 1)
+      string match -q -r "^[\sACMRT]D\s*" $line;       and set -l deletions_unstaged (math $deletions_unstaged + 1)
+      string match -q -r "^D[DU]\s*" $line;            and set -l deletions_ours (math $deletions_ours + 1)
+      string match -q -r "^[DU]D\s*" $line;            and set -l deletions_theirs (math $deletions_theirs + 1)
 
       # Compute additions
-      string match -q -r "^[AC][\sDMT]\s*" $line;      and set additions_staged (math $additions_staged + 1)
-      string match -q -r "^\?\?\s*" $line;             and set additions_unstaged (math $additions_unstaged + 1)
-      string match -q -r "^A[AU]\s*" $line;            and set additions_ours (math $additions_ours + 1)
-      string match -q -r "^[AU]A\s*" $line;            and set additions_theirs (math $additions_theirs + 1)
+      string match -q -r "^[AC][\sDMT]\s*" $line;      and set -l additions_staged (math $additions_staged + 1)
+      string match -q -r "^\?\?\s*" $line;             and set -l additions_unstaged (math $additions_unstaged + 1)
+      string match -q -r "^A[AU]\s*" $line;            and set -l additions_ours (math $additions_ours + 1)
+      string match -q -r "^[AU]A\s*" $line;            and set -l additions_theirs (math $additions_theirs + 1)
 
       # Compute modifications
-      string match -q -r "^[MRT][\sDMT]\s*" $line;     and set modifications_staged (math $modifications_staged + 1)
-      string match -q -r "^[\sACMDRT][MRT]\s*" $line;  and set modifications_unstaged (math $modifications_unstaged + 1)
-      string match -q -r "^U.\s*" $line;               and set modifications_ours (math $modifications_ours + 1)
-      string match -q -r "^.U\s*" $line;               and set modifications_theirs (math $modifications_theirs + 1)
+      string match -q -r "^[MRT][\sDMT]\s*" $line;     and set -l modifications_staged (math $modifications_staged + 1)
+      string match -q -r "^[\sACMDRT][MRT]\s*" $line;  and set -l modifications_unstaged (math $modifications_unstaged + 1)
+      string match -q -r "^U.\s*" $line;               and set -l modifications_ours (math $modifications_ours + 1)
+      string match -q -r "^.U\s*" $line;               and set -l modifications_theirs (math $modifications_theirs + 1)
 
     end
 
     # Number of all changes
-    set changes (math $deletions_staged + $deletions_unstaged +  $additions_staged + $additions_unstaged + $modifications_staged + $modifications_unstaged)
+    set -l changes (math $deletions_staged + $deletions_unstaged +  $additions_staged + $additions_unstaged + $modifications_staged + $modifications_unstaged)
 
     # Render deletions
     if test $changes -gt 0
       if test $deletions_staged -gt 0; and test $deletions_unstaged -gt 0
-        echo -n -s (set_color yellow) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $partially_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $deletions_staged -gt 0
-        echo -n -s (set_color green) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $fully_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $deletions_unstaged -gt 0
-        echo -n -s (set_color red) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $none_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else
-        echo -n -s (set_color magenta) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $nothing_to_stage_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       end
     else
-      echo -n -s (set_color magenta --dim) "❮" (set_color normal)
+      echo -n -s \
+        (set_color $nothing_to_stage_color --dim --background=$background_reset_color)\
+        $git_index_badge\
+        (set_color $reset_color --background=$background_reset_color)
     end
 
     # Render additions
     if test $changes -gt 0
       if test $additions_staged -gt 0; and test $additions_unstaged -gt 0
-        echo -n -s (set_color yellow) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $partially_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $additions_staged -gt 0
-        echo -n -s (set_color green) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $fully_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $additions_unstaged -gt 0
-        echo -n -s (set_color red) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $none_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else
-        echo -n -s (set_color magenta) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $nothing_to_stage_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       end
     else
-      echo -n -s (set_color magenta --dim) "❮" (set_color normal)
+      echo -n -s \
+        (set_color $nothing_to_stage_color --dim --background=$background_reset_color)\
+        $git_index_badge\
+        (set_color $reset_color --background=$background_reset_color)
     end
 
     # Render modifications
     if test $changes -gt 0
       if test $modifications_staged -gt 0; and test $modifications_unstaged -gt 0
-        echo -n -s (set_color yellow) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $partially_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $modifications_staged -gt 0
-        echo -n -s (set_color green) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $fully_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else if test $modifications_unstaged -gt 0
-        echo -n -s (set_color red) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $none_staged_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       else
-        echo -n -s (set_color magenta) "❮" (set_color normal)
+        echo -n -s \
+          (set_color $nothing_to_stage_color --background=$background_reset_color)\
+          $git_index_badge\
+          (set_color $reset_color --background=$background_reset_color)
       end
     else
-      echo -n -s (set_color magenta --dim) "❮" (set_color normal)
+      echo -n -s \
+        (set_color $nothing_to_stage_color --dim --background=$background_reset_color)\
+        $git_index_badge\
+        (set_color $reset_color --background=$background_reset_color)
     end
 
   else
     # Not in a git dir; display static chevrons
-    echo -n -s (set_color magenta --dim) "❮❮❮" (set_color normal)
+    echo -n -s \
+      (set_color $nothing_to_stage_color --dim --background=$background_reset_color)\
+      $git_index_badge$git_index_badge$git_index_badge\
+      (set_color $reset_color --background=$background_reset_color)
   end
 
   if git rev-parse --is-inside-work-tree > /dev/null 2>&1
     echo -n -s ' '
 
     ####
-    # HEAD status
+    # GIT HEAD status
     ##
 
-    set short_sha (git rev-parse -q --short HEAD)
-    set branch (git symbolic-ref -q --short HEAD)
+    set -l short_sha (git rev-parse -q --short HEAD)
+    set -l branch (git branch --show-current)
 
     if test -n "$branch"
-      echo -n -s (set_color cyan --italics --background=brblack) $branch (set_color normal)
+      echo -n -s \
+        (set_color cyan --italics --background=$background_reset_color)\
+        $branch\
+        (set_color $reset_color --background=$background_reset_color)
     else
-      echo -n -s (set_color red --bold --italics --background=brblack) $short_sha (set_color normal)
+      echo -n -s (set_color red --bold --italics --background=$background_reset_color)\
+      $short_sha\
+      (set_color $reset_color --background=$background_reset_color)
     end
   end
 
   echo -n -s ' '
 
-  # show last command run duration
+  ####
+  # LAST COMMAND status
+  ##
 
-  if test $last -gt 0
-    echo -n -s (set_color white --background="red") (cmd_duration_readable) (set_color normal)
+  set -l foreground_status_color white
+  set -l background_status_failed_color red
+  set -l background_status_succeeded_color $background_reset_color
+
+  # show last command run duration colored by exit code
+  if test $status -gt 0
+    echo -n -s \
+      (set_color $foreground_status_color --background=$background_status_failed_color)\
+      (cmd_duration_readable)\
+      (set_color $background_status_succeeded_color)
   else
-    echo -n -s (set_color white --dim) (cmd_duration_readable) (set_color normal)
+    echo -n -s \
+      (set_color $foreground_status_color --dim --background=$background_status_succeeded_color)\
+      (cmd_duration_readable)\
+      (set_color $background_status_succeeded_color)
   end
 end
